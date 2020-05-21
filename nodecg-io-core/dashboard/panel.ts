d8591ca65cfc6b9d56334fba45e85e767ef6080d
Daniel <daniel.hub@outlook.de>
2020-05-21 10:19:45 +0200

    Initial commit

---
+++ nodecg-io-core/dashboard/panel.ts
@@ -0,0 +1,2 @@
+/// <reference types="nodecg/types/browser" />
+
---
+++ nodecg-io-core/extension/index.ts
@@ -0,0 +1,5 @@
+import { NodeCG } from 'nodecg/types/server'
+
+module.exports = function (nodecg: NodeCG) {
+	nodecg.log.info('Minzig!');
+};
