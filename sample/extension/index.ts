129d94857a3cc31568271adf0ff1104f561e3413
Daniel <daniel.hub@outlook.de>
2020-06-03 09:40:59 +0200

    Add a sample bundle using twitch for testing

---
+++ sample/extension/index.ts
@@ -0,0 +1,17 @@
+import { NodeCG } from "nodecg/types/server";
+import { ServiceProvider } from "nodecg-io-core/extension/types";
+import { TwitchClient } from "nodecg-io-twitch/extension";
+
+module.exports = function (nodecg: NodeCG) {
+    nodecg.log.info("Sample bundle for twitch started");
+    const twitch: ServiceProvider<TwitchClient> = nodecg.extensions["nodecg-io-twitch"] as any;
+
+    twitch.registerBundle("sample", (client) => {
+        if (client === undefined) {
+			nodecg.log.info("Twitch client has been unset.");
+        } else {
+			nodecg.log.info("Twitch client has been updated.");
+			nodecg.log.info(JSON.stringify(client));
+        }
+    });
+};
