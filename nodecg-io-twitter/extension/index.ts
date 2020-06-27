4d4cfea0e46fc02204810dd50e68164954f1d527
LarsVomMars <lars@kroenner.eu>
2020-06-27 22:50:33 +0200

    Eslint

--- nodecg-io-twitter/extension/index.ts
+++ nodecg-io-twitter/extension/index.ts
@@ -2,18 +2,17 @@ import { NodeCG } from "nodecg/types/server";
 import { NodeCGIOCore } from "nodecg-io-core/extension";
 import { Service, ServiceProvider } from "nodecg-io-core/extension/types";
 import { emptySuccess, success, error, Result } from "nodecg-io-core/extension/utils/result";
-const Twitter = require("twitter");
-
+const Twitter = require("twitter"); // eslint-disable-line @typescript-eslint/no-var-requires
 
 interface TwitterServiceConfig {
-	oauthConsumerKey: string,
-	oauthConsumerSecret: string,
-	oauthToken: string,
-	oauthTokenSecret: string,
+    oauthConsumerKey: string;
+    oauthConsumerSecret: string;
+    oauthToken: string;
+    oauthTokenSecret: string;
 }
 
 export interface TwitterServiceClient {
-    getRawClient(): typeof Twitter
+    getRawClient(): typeof Twitter;
 }
 
 module.exports = (nodecg: NodeCG): ServiceProvider<TwitterServiceClient> | undefined => {
@@ -29,53 +28,57 @@ module.exports = (nodecg: NodeCG): ServiceProvider<TwitterServiceClient> | undef
         serviceType: "twitter",
         validateConfig: validateConfig,
         createClient: createClient(nodecg),
-        stopClient: stopClient
+        stopClient: stopClient,
     };
 
     return core.registerService(service);
 };
 
 async function validateConfig(config: TwitterServiceConfig): Promise<Result<void>> {
-	try {
-		// Try to connect to twitter
-		const client = new Twitter({
-			consumer_key: config.oauthConsumerKey,
-			consumer_secret: config.oauthConsumerSecret,
-			access_token_key: config.oauthToken,
-			access_token_secret: config.oauthTokenSecret,
-		});
-		// Validate credentials
-		const resp = await client.get('account/verify_credentials', {});
-		return emptySuccess();
-	} catch (err) {
-		return error(err[0].message);
-	}
+    try {
+        // Try to connect to twitter
+        const client = new Twitter({
+            consumer_key: config.oauthConsumerKey, // eslint-disable-line camelcase
+            consumer_secret: config.oauthConsumerSecret, // eslint-disable-line camelcase
+            access_token_key: config.oauthToken, // eslint-disable-line camelcase
+            access_token_secret: config.oauthTokenSecret, // eslint-disable-line camelcase
+        });
+        console.log(Twitter);
+        // Validate credentials
+        await client.get("account/verify_credentials", {});
+        return emptySuccess();
+    } catch (err) {
+        if (err[0] && "message" in err[0]) {
+            return error(err[0].message);
+        }
+
+        return error(error);
+    }
 }
 
 function createClient(nodecg: NodeCG): (config: TwitterServiceConfig) => Promise<Result<TwitterServiceClient>> {
     return async (config) => {
-		try {
-			nodecg.log.info("Connecting to twitter ...");
-			const client = new Twitter({
-				consumer_key: config.oauthConsumerKey,
-				consumer_secret: config.oauthConsumerSecret,
-				access_token_key: config.oauthToken,
-				access_token_secret: config.oauthTokenSecret,
-			});
-			nodecg.log.info("Successfully connected to twitter!")
+        try {
+            nodecg.log.info("Connecting to twitter ...");
+            const client = new Twitter({
+                consumer_key: config.oauthConsumerKey, // eslint-disable-line camelcase
+                consumer_secret: config.oauthConsumerSecret, // eslint-disable-line camelcase
+                access_token_key: config.oauthToken, // eslint-disable-line camelcase
+                access_token_secret: config.oauthTokenSecret, // eslint-disable-line camelcase
+            });
+            nodecg.log.info("Successfully connected to twitter!");
 
             return success({
                 getRawClient() {
                     return client;
-                }
+                },
             });
-
-		} catch (err) {
+        } catch (err) {
             return error(err.toString());
-		}
+        }
     };
 }
 
 function stopClient(client: TwitterServiceClient): void {
-	// You are not really able to stop the client ...
+    // You are not really able to stop the client ...
 }
