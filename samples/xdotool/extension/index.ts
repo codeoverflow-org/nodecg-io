dfe8463495552e2ee58570138f9cc07e58ea54e8
noeppi_noeppi <noeppinoeppi@gmail.com>
2020-07-07 22:26:22 +0200

    Replaced one occurence of 'twitch' with 'xdotool'

--- samples/xdotool/extension/index.ts
+++ samples/xdotool/extension/index.ts
@@ -3,7 +3,7 @@ import { ServiceProvider } from "nodecg-io-core/extension/types";
 import { XdotoolServiceClient } from "nodecg-io-xdotool/extension";
 
 module.exports = function (nodecg: NodeCG) {
-    nodecg.log.info("Sample bundle for twitch started");
+    nodecg.log.info("Sample bundle for xdotool started");
 
     // This explicit cast determines the client type in the requireService call
     const xdotool = (nodecg.extensions["nodecg-io-xdotool"] as unknown) as
