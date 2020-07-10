e6f92562b93416e7e8a1d9533f65e2050baaae5f
Daniel <daniel.hub@outlook.de>
2020-07-10 19:00:06 +0200

    Fix formatting of ws server sample

--- samples/websocket-server/extension/index.ts
+++ samples/websocket-server/extension/index.ts
@@ -10,22 +10,21 @@ module.exports = function (nodecg: NodeCG) {
         | ServiceProvider<WSServerServiceClient>
         | undefined;
 
-        webSocketServer?.requireService(
+    webSocketServer?.requireService(
         "websocket-server",
         (client) => {
             nodecg.log.info("WebSocket Server has been set. Ready for connections.");
 
             const server = client.getRawClient();
-            server.on('connection', (websocket) => {
-                websocket.on('message', (message) => {
-                    if(message.toString().toLowerCase() === "ping") {
-                        websocket.send('pong');
-                    }else{
+            server.on("connection", (websocket) => {
+                websocket.on("message", (message) => {
+                    if (message.toString().toLowerCase() === "ping") {
+                        websocket.send("pong");
+                    } else {
                         server.clients.forEach((client) => client.send(message));
                     }
-                })
-            })
-
+                });
+            });
         },
         () => nodecg.log.info("WebSocket Server has been unset!"),
     );
