aef400b5fe5c328eeae13eb492dc30fdaeb24848
Niklas <derNiklaas@users.noreply.github.com>
2020-07-28 14:17:53 +0200

    Added requested changes

--- samples/streamelements/extension/index.ts
+++ samples/streamelements/extension/index.ts
@@ -17,16 +17,12 @@ module.exports = function (nodecg: NodeCG) {
 
             client.onCheer((data) => {
                 nodecg.log.info(
-                    data.data.displayName +
-                        " just cheered " +
-                        data.data.amount +
-                        " bit(s). Message: " +
-                        data.data.message,
+                    `${data.data.displayName} just cheered ${data.data.amount} bit(s). Message: ${data.data.message}`,
                 );
             });
 
             client.onFollow((data) => {
-                nodecg.log.info(data.data.displayName + " just followed!");
+                nodecg.log.info(`${data.data.displayName} just followed.`);
             });
 
             client.onSubscriber((data) => {
@@ -34,7 +30,7 @@ module.exports = function (nodecg: NodeCG) {
                     const tier =
                         data.data.tier === "prime" ? "Twitch Prime" : "Tier " + Number.parseInt(data.data.tier) / 1000;
                     nodecg.log.info(
-                        data.data.displayName + " just subscribed for " + data.data.amount + " months (" + tier + ")!",
+                        `${data.data.displayName} just subscribed for ${data.data.amount} months (${tier}).`,
                     );
                 }
             });
@@ -44,48 +40,28 @@ module.exports = function (nodecg: NodeCG) {
                     const tier = (Number.parseInt(data.data.tier) / 1000).toString();
                     if (data.data.sender) {
                         nodecg.log.info(
-                            data.data.displayName +
-                                " just got a tier " +
-                                tier +
-                                " subscribtion from" +
-                                data.data.sender +
-                                "! It's " +
-                                data.data.displayName +
-                                "'s " +
-                                data.data.amount +
-                                " month.",
+                            `${data.data.displayName} just got a tier ${tier} subscription from ${data.data.sender}! It's ${data.data.displayName}'s ${data.data.amount} month.`,
                         );
                     } else {
                         nodecg.log.info(
-                            data.data.displayName +
-                                " just got a tier " +
-                                tier +
-                                " gift subscribtion! It's " +
-                                data.data.displayName +
-                                "'s " +
-                                data.data.amount +
-                                " month.",
+                            `${data.data.displayName} just got a tier ${tier} subscription! It's ${data.data.displayName}'s ${data.data.amount} month.`,
                         );
                     }
                 }
             });
 
             client.onHost((data) => {
-                nodecg.log.info(
-                    data.data.displayName + " just hosted the stream for " + data.data.amount + "viewer(s)!",
-                );
+                nodecg.log.info(`${data.data.displayName} just hosted the stream for ${data.data.amount} viewer(s).`);
             });
 
             client.onRaid((data) => {
-                nodecg.log.info(
-                    data.data.displayName + " just raided the stream with " + data.data.amount + " viewers!",
-                );
+                nodecg.log.info(`${data.data.displayName} just raided the stream with ${data.data.amount} viewers.`);
             });
 
             client.onTip((data) => {
                 if (data.data.currency) {
                     nodecg.log.info(
-                        data.data.username + " just donated " + data.data.amount + " " + data.data.currency + "!",
+                        `${data.data.username} just donated ${data.data.amount} ${data.data.currency}. Message. ${data.data.message}`,
                     );
                 }
             });
