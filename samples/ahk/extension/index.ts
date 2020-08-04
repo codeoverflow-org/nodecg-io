ee8432dba059bbecb1a59496391f83e654d50804
Daniel <daniel.hub@outlook.de>
2020-08-04 15:14:41 +0200

    Fix wrong service type in ahk sample

--- samples/ahk/extension/index.ts
+++ samples/ahk/extension/index.ts
@@ -5,7 +5,7 @@ import { requireService } from "nodecg-io-core/extension/serviceClientWrapper";
 module.exports = function (nodecg: NodeCG) {
     nodecg.log.info("Sample bundle for AHK started");
 
-    const ahk = requireService<AHKServiceClient>(nodecg, "twitch");
+    const ahk = requireService<AHKServiceClient>(nodecg, "ahk");
 
     ahk?.onAvailable((client) => {
         nodecg.log.info("AHK client has been updated, sending Hello World Command.");
