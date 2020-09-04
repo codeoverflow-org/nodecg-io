26ab1733c7681befc4115fa4ef6069c07cd23347
Daniel <daniel.hub@outlook.de>
2020-09-04 15:05:52 +0200

    Use twitch-auth and update to non deprecated twitch functions

--- nodecg-io-twitch/extension/index.ts
+++ nodecg-io-twitch/extension/index.ts
@@ -2,8 +2,8 @@ import { NodeCG } from "nodecg/types/server";
 import { ServiceClient } from "nodecg-io-core/extension/types";
 import { emptySuccess, Result, success } from "nodecg-io-core/extension/utils/result";
 import { ServiceBundle } from "nodecg-io-core/extension/serviceBundle";
-import TwitchClient from "twitch";
-import ChatClient from "twitch-chat-client";
+import { getTokenInfo, StaticAuthProvider } from "twitch-auth";
+import { ChatClient } from "twitch-chat-client";
 
 interface TwitchServiceConfig {
     oauthKey: string;
@@ -18,25 +18,28 @@ module.exports = (nodecg: NodeCG) => {
 class TwitchService extends ServiceBundle<TwitchServiceConfig, TwitchServiceClient> {
     async validateConfig(config: TwitchServiceConfig): Promise<Result<void>> {
         const authKey = config.oauthKey.replace("oauth:", "");
-        await TwitchClient.getTokenInfo(authKey); // This will throw a error if the token is invalid
+        await getTokenInfo(authKey); // This will throw a error if the token is invalid
         return emptySuccess();
     }
 
     async createClient(config: TwitchServiceConfig): Promise<Result<TwitchServiceClient>> {
         // This twitch client needs the token without the "oauth:" before the actual token, strip it away
         const authKey = config.oauthKey.replace("oauth:", "");
-        // Create a twitch authentication client
-        const tokenInfo = await TwitchClient.getTokenInfo(authKey);
-        const authClient = TwitchClient.withCredentials(tokenInfo.clientId, authKey, tokenInfo.scopes);
+
+        // Create a twitch authentication provider
+        const tokenInfo = await getTokenInfo(authKey);
+        const authProvider = new StaticAuthProvider(tokenInfo.clientId, authKey, tokenInfo.scopes);
 
         // Create the actual chat client and connect
-        const chatClient = ChatClient.forTwitchClient(authClient);
+        const chatClient = new ChatClient(authProvider);
         this.nodecg.log.info("Connecting to twitch chat...");
         await chatClient.connect(); // Connects to twitch IRC
+
         // This also waits till it has registered itself at the IRC server, which is needed to do anything.
         await new Promise((resolve, _reject) => {
             chatClient.onRegister(resolve);
         });
+
         this.nodecg.log.info("Successfully connected to twitch.");
 
         return success({
