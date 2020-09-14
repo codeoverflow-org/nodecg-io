a4aa3767ad7de8728abb67784e6113e9d8b3284b
StefanSchmelz <stefan_schmelz@gmx.net>
2020-09-14 14:34:11 +0200

    Made the sample send midi messages on channels 0 and 1 randomly every second

--- samples/midi-output/extension/index.ts
+++ samples/midi-output/extension/index.ts
@@ -1,6 +1,7 @@
 import { NodeCG } from "nodecg/types/server";
 import { requireService } from "nodecg-io-core/extension/serviceClientWrapper";
 import { MidiOutputServiceClient } from "nodecg-io-midi-output/extension";
+import { Note, Channel } from "easymidi";
 
 module.exports = function (nodecg: NodeCG) {
     nodecg.log.info("Sample bundle for midi-output started");
@@ -8,8 +9,20 @@ module.exports = function (nodecg: NodeCG) {
     const service = requireService<MidiOutputServiceClient>(nodecg, "midi-output");
     service?.onAvailable((client) => {
         nodecg.log.info("Midioutput client has been updated.");
+        const midiClient = client.getNativeClient();
 
-        // TODO do something with the client to demonstrate the functionality.
+        setInterval(() => {
+            let noteVal: number = Math.round(Math.random() * 126);
+            let velocityVal: number = Math.round(Math.random() * 126);
+            let channelVal: Channel = <Channel>Math.round(Math.random() * 1);
+
+            const data: Note = {
+                note: noteVal,
+                velocity: velocityVal,
+                channel: 0,
+            };
+            midiClient.send("noteon", data);
+        }, 1000);
     });
 
     service?.onUnavailable(() => nodecg.log.info("Midi output client has been unset."));
