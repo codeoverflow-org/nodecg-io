170c20583c701172bcd24e1e1c8b1ce6c2ce5730
StefanSchmelz <stefan_schmelz@gmx.net>
2020-09-20 21:58:57 +0200

    improved midi devices

--- nodecg-io-midi-input/extension/index.ts
+++ nodecg-io-midi-input/extension/index.ts
@@ -1,6 +1,6 @@
 import { NodeCG } from "nodecg/types/server";
 import { ServiceClient } from "nodecg-io-core/extension/types";
-import { emptySuccess, Result, success } from "nodecg-io-core/extension/utils/result";
+import { emptySuccess, Result, success, error } from "nodecg-io-core/extension/utils/result";
 import { ServiceBundle } from "nodecg-io-core/extension/serviceBundle";
 import * as easymidi from "easymidi";
 
@@ -16,12 +16,34 @@ module.exports = (nodecg: NodeCG) => {
 
 class MidiService extends ServiceBundle<MidiInputServiceConfig, MidiInputServiceClient> {
     async validateConfig(config: MidiInputServiceConfig): Promise<Result<void>> {
-        new easymidi.Input(config.device).close();
+        let devices: Array<string> = new Array<string>();
+
+        easymidi.getInputs().forEach((device) => {
+            if (device.includes(config.device)) {
+                devices.push(device);
+            }
+        });
+
+        if (devices.length == 0) {
+            return error("no device matched the configured pattern.");
+        }
+        if (devices.length > 1) {
+            return error("The configured pattern is ambiguous.");
+        }
         return emptySuccess();
     }
 
     async createClient(config: MidiInputServiceConfig): Promise<Result<MidiInputServiceClient>> {
-        this.nodecg.log.info(`Connecting to MIDI input device ${config.device}.`);
+        this.nodecg.log.info(`Checking device name "${config.device}"`);
+        let devices: Array<string> = new Array<string>();
+        let deviceName: string | null = null;
+        easymidi.getInputs().forEach((device) => {
+            if (device.includes(config.device)) {
+                deviceName = device;
+            }
+        });
+
+        this.nodecg.log.info(`Connecting to MIDI input device ${deviceName}.`);
         const client = new easymidi.Input(config.device);
         this.nodecg.log.info(`Successfully connected to MIDI input device ${config.device}.`);
 
--- nodecg-io-midi-output/extension/index.ts
+++ nodecg-io-midi-output/extension/index.ts
@@ -1,6 +1,6 @@
 import { NodeCG } from "nodecg/types/server";
 import { ServiceClient } from "nodecg-io-core/extension/types";
-import { emptySuccess, Result, success } from "nodecg-io-core/extension/utils/result";
+import { emptySuccess, Result, success, error } from "nodecg-io-core/extension/utils/result";
 import { ServiceBundle } from "nodecg-io-core/extension/serviceBundle";
 import * as easymidi from "easymidi";
 
@@ -16,11 +16,33 @@ module.exports = (nodecg: NodeCG) => {
 
 class MidiService extends ServiceBundle<MidiOutputServiceConfig, MidiOutputServiceClient> {
     async validateConfig(config: MidiOutputServiceConfig): Promise<Result<void>> {
-        new easymidi.Output(config.device).close();
+        let devices: Array<string> = new Array<string>();
+
+        easymidi.getInputs().forEach((device) => {
+            if (device.includes(config.device)) {
+                devices.push(device);
+            }
+        });
+
+        if (devices.length == 0) {
+            return error("no device matched the configured pattern.");
+        }
+        if (devices.length > 1) {
+            return error("The configured pattern is ambiguous.");
+        }
         return emptySuccess();
     }
 
     async createClient(config: MidiOutputServiceConfig): Promise<Result<MidiOutputServiceClient>> {
+        this.nodecg.log.info(`Checking device name "${config.device}"`);
+        let devices: Array<string> = new Array<string>();
+        let deviceName: string | null = null;
+        easymidi.getOutputs().forEach((device) => {
+            if (device.includes(config.device)) {
+                deviceName = device;
+            }
+        });
+
         this.nodecg.log.info(`Connecting to MIDI output device ${config.device}.`);
         const client = new easymidi.Output(config.device);
         this.nodecg.log.info(`Successfully connected to MIDI output device ${config.device}.`);
