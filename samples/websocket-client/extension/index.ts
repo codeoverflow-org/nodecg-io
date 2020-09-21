e1133c6997ea2b0fc847d058d49303f4798b66e5
StefanSchmelz <stefan_schmelz@gmx.net>
2020-09-21 11:48:04 +0200

    Created websocket sample

---
+++ samples/websocket-client/extension/index.ts
@@ -0,0 +1,23 @@
+import { NodeCG } from "nodecg/types/server";
+import { requireService } from "nodecg-io-core/extension/serviceClientWrapper";
+import { WSClientServiceClient } from "nodecg-io-websocket-client/extension";
+
+module.exports = function (nodecg: NodeCG) {
+    nodecg.log.info("Sample bundle for <the-service-name> started");
+
+    const service = requireService<WSClientServiceClient>(nodecg, "websocket-client");
+    service?.onAvailable((client) => {
+        nodecg.log.info("Client has been updated.");
+
+        const sock = client.getNativeClient();
+        sock.addListener("message", (code: number, data: string) => {
+            nodecg.log.info(`recieved message "${data}" with code: ${code}`);
+        });
+        setInterval(() => {
+            nodecg.log.info("Sending ping ...");
+            sock.send("ping");
+        }, 1000);
+    });
+
+    service?.onUnavailable(() => nodecg.log.info("Client has been unset."));
+};
