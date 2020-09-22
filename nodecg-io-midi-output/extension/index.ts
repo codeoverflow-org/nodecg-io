51bd734b1fdbbbd29d80b6676235bc1bb9de7bad
Stefan Schmelz <35258671+StefanSchmelz@users.noreply.github.com>
2020-09-22 19:42:47 +0200

    Bug/midi device name fix (#117)
    * improved midi devices
    
    * fixed patterns becoming ambiguous wiothout config change
    
    * fixed most linker problems
    
    * Update nodecg-io-midi-input/extension/index.ts
    
    Co-authored-by: Daniel Huber <daniel.hub@outlook.de>
    
    * Update nodecg-io-midi-input/extension/index.ts
    
    Okay that capitalisation sailöed right over my head... Thanks for the correction
    
    Co-authored-by: Daniel Huber <daniel.hub@outlook.de>
    
    * Update nodecg-io-midi-input/extension/index.ts
    
    Co-authored-by: Daniel Huber <daniel.hub@outlook.de>
    
    * Fixed some potential problems.
    
    * fixed tripple equals
    
    * still a problem
    
    Co-authored-by: Daniel Huber <daniel.hub@outlook.de>

--- nodecg-io-midi-output/extension/index.ts
+++ nodecg-io-midi-output/extension/index.ts
@@ -1,6 +1,6 @@
 import { NodeCG } from "nodecg/types/server";
 import { ServiceClient } from "nodecg-io-core/extension/types";
-import { emptySuccess, Result, success } from "nodecg-io-core/extension/utils/result";
+import { emptySuccess, Result, success, error } from "nodecg-io-core/extension/utils/result";
 import { ServiceBundle } from "nodecg-io-core/extension/serviceBundle";
 import * as easymidi from "easymidi";
 
@@ -16,20 +16,45 @@ module.exports = (nodecg: NodeCG) => {
 
 class MidiService extends ServiceBundle<MidiOutputServiceConfig, MidiOutputServiceClient> {
     async validateConfig(config: MidiOutputServiceConfig): Promise<Result<void>> {
-        new easymidi.Output(config.device).close();
+        const devices: Array<string> = new Array<string>();
+
+        easymidi.getInputs().forEach((device) => {
+            if (device.includes(config.device)) {
+                devices.push(device);
+            }
+        });
+
+        if (devices.length === 0) {
+            return error("no device matched the configured pattern.");
+        }
+        if (devices.length > 1) {
+            return error("The configured pattern is ambiguous.");
+        }
         return emptySuccess();
     }
 
     async createClient(config: MidiOutputServiceConfig): Promise<Result<MidiOutputServiceClient>> {
-        this.nodecg.log.info(`Connecting to MIDI output device ${config.device}.`);
-        const client = new easymidi.Output(config.device);
-        this.nodecg.log.info(`Successfully connected to MIDI output device ${config.device}.`);
-
-        return success({
-            getNativeClient() {
-                return client;
-            },
+        this.nodecg.log.info(`Checking device name "${config.device}"`);
+        let deviceName: string | null = null;
+        easymidi.getOutputs().forEach((device) => {
+            if (device.includes(config.device) && deviceName === null) {
+                deviceName = device;
+            }
         });
+
+        this.nodecg.log.info(`Connecting to MIDI output device ${deviceName}.`);
+        if (deviceName !== null) {
+            const client = new easymidi.Output(deviceName);
+            this.nodecg.log.info(`Successfully connected to MIDI output device ${deviceName}.`);
+
+            return success({
+                getNativeClient() {
+                    return client;
+                },
+            });
+        } else {
+            return error("Could not connect to the configured device.");
+        }
     }
 
     stopClient(client: MidiOutputServiceClient): void {
