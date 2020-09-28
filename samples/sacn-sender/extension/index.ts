2a1d670e13b4e2f33092daf8891a101ab863aab8
StefanSchmelz <stefan_schmelz@gmx.net>
2020-09-28 11:38:55 +0200

    Created sacn sender sample

---
+++ samples/sacn-sender/extension/index.ts
@@ -0,0 +1,24 @@
+import { NodeCG } from "nodecg/types/server";
+import { requireService } from "nodecg-io-core/extension/serviceClientWrapper";
+import { SacnSenderServiceClient } from "nodecg-io-sacn-sender/extension";
+
+module.exports = function (nodecg: NodeCG) {
+    nodecg.log.info("Sample bundle for sacn-sender started");
+
+    const service = requireService<SacnSenderServiceClient>(nodecg, "sacn-sender");
+    service?.onAvailable((client) => {
+        nodecg.log.info("sacn-sender has been updated.");
+
+        setInterval(() => {
+            const channel = Math.round(Math.random() * 512).toString();
+            const value = Math.round(Math.random() * 100);
+            const payload = {
+                [channel]: value,
+            };
+            nodecg.log.info("Sending " + value + " to channel #" + channel);
+            client.sendPayload(payload);
+        }, 10000);
+    });
+
+    service?.onUnavailable(() => nodecg.log.info("sacn-sender has been unset."));
+};
