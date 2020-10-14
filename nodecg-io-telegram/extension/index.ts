9d2e74a8cfdbe596fbe8ebf5c86fc428c0cd1047
Daniel <daniel.hub@outlook.de>
2020-10-14 20:15:37 +0200

    Implement removeHandlers for telegram

--- nodecg-io-telegram/extension/index.ts
+++ nodecg-io-telegram/extension/index.ts
@@ -4,6 +4,23 @@ import { ServiceBundle } from "nodecg-io-core/extension/serviceBundle";
 import TelegramBot = require("node-telegram-bot-api");
 import { ServiceClient } from "nodecg-io-core/extension/types";
 
+// TODO: try to upstream these changes to the original package so they can be accessed using public methods
+// instead of having to access private variables.
+class BetterTelegramBot extends TelegramBot {
+    private declare _textRegexpCallbacks: Array<RegExp>;
+    private declare _replyListeners: Array<unknown>;
+
+    clearTextListeners() {
+        // The class currently doesn't have a way to reset all regex listeners.
+        this._textRegexpCallbacks = [];
+    }
+
+    clearReplyListeners() {
+        // The class currently also doesn't have a way to reset all reply listeners.
+        this._replyListeners = [];
+    }
+}
+
 interface TelegramServiceConfig {
     token: string;
     polling?: boolean;
@@ -13,7 +30,7 @@ interface TelegramServiceConfig {
     filepath?: boolean;
 }
 
-export type TelegramServiceClient = ServiceClient<TelegramBot>;
+export type TelegramServiceClient = ServiceClient<BetterTelegramBot>;
 
 module.exports = (nodecg: NodeCG) => {
     new TelegramService(nodecg, "telegram", __dirname, "../telegram-schema.json").register();
@@ -36,7 +53,7 @@ class TelegramService extends ServiceBundle<TelegramServiceConfig, TelegramServi
             webHook: config.webHook,
         };
 
-        const bot = new TelegramBot(config.token, options);
+        const bot = new BetterTelegramBot(config.token, options);
 
         this.nodecg.log.info("Successfully connected to the telegram server.");
 
@@ -54,5 +71,11 @@ class TelegramService extends ServiceBundle<TelegramServiceConfig, TelegramServi
         if (client.getNativeClient().hasOpenWebHook()) {
             client.getNativeClient().closeWebHook();
         }
+    }
+
+    removeHandlers(client: TelegramServiceClient): void {
+        client.getNativeClient().removeAllListeners();
+        client.getNativeClient().clearTextListeners();
+        client.getNativeClient().clearReplyListeners();
     }
 }
