aeb5a0bbdd61560f0a8e3587cb71dd03fe325c01
StefanSchmelz <stefan_schmelz@gmx.net>
2020-10-21 22:04:06 +0200

    current progress

---
+++ samples/serial/extension/index.ts
@@ -0,0 +1,21 @@
+import { NodeCG } from "nodecg/types/server";
+import { requireService } from "nodecg-io-core/extension/serviceClientWrapper";
+import { SerialServiceClient } from "nodecg-io-serial/extension";
+
+module.exports = function (nodecg: NodeCG) {
+    nodecg.log.info("Sample bundle for serial started");
+
+    const service = requireService<SerialServiceClient>(nodecg, "serial");
+    service?.onAvailable((client) => {
+        nodecg.log.info("Client has been updated.");
+        client.onData((data) => {
+            nodecg.log.info(data);
+        });
+
+        setInterval(() => {
+            client.send("ping");
+        }, 10000);
+    });
+
+    service?.onUnavailable(() => nodecg.log.info("Client has been unset."));
+};
