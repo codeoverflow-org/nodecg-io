64f574c2986f72322cca951927c0521363fc4a46
StefanSchmelz <stefan_schmelz@gmx.net>
2020-11-04 22:48:32 +0100

    updated serial sample to match current service api

--- samples/serial/extension/index.ts
+++ samples/serial/extension/index.ts
@@ -8,15 +8,13 @@ module.exports = function (nodecg: NodeCG) {
     const service = requireService<SerialServiceClient>(nodecg, "serial");
     service?.onAvailable((client) => {
         nodecg.log.info("Client has been updated.");
-        client.init().then(() => {
-            client.onData((data: string) => {
-                nodecg.log.info(data);
-            });
-
-            setInterval(() => {
-                client.send("ping\n");
-            }, 10000);
+        client.onData((data: string) => {
+            nodecg.log.info(data);
         });
+
+        setInterval(() => {
+            client.send("ping\n");
+        }, 10000);
     });
 
     service?.onUnavailable(() => nodecg.log.info("Client has been unset."));
