a7f8159b07e141d3bbab4018b033e83204712447
Daniel <daniel.hub@outlook.de>
2021-01-01 13:21:22 +0100

    Update typings for telegram lib and use upstreamed clear listener methods

--- nodecg-io-telegram/extension/index.ts
+++ nodecg-io-telegram/extension/index.ts
@@ -4,22 +4,6 @@ import { ServiceBundle } from "nodecg-io-core/extension/serviceBundle";
 import TelegramBot = require("node-telegram-bot-api");
 import { ServiceClient } from "nodecg-io-core/extension/types";
 
-// TODO: These changes are now upstreamed (https://github.com/yagop/node-telegram-bot-api/commit/ec7e61e041a66a52a90be0849c3d4640a4806873) but typing still need updating
-class BetterTelegramBot extends TelegramBot {
-    private declare _textRegexpCallbacks: Array<RegExp>;
-    private declare _replyListeners: Array<unknown>;
-
-    clearTextListeners() {
-        // The class currently doesn't have a way to reset all regex listeners.
-        this._textRegexpCallbacks = [];
-    }
-
-    clearReplyListeners() {
-        // The class currently also doesn't have a way to reset all reply listeners.
-        this._replyListeners = [];
-    }
-}
-
 interface TelegramServiceConfig {
     token: string;
     polling?: boolean;
@@ -29,7 +13,7 @@ interface TelegramServiceConfig {
     filepath?: boolean;
 }
 
-export type TelegramServiceClient = ServiceClient<BetterTelegramBot>;
+export type TelegramServiceClient = ServiceClient<TelegramBot>;
 
 module.exports = (nodecg: NodeCG) => {
     new TelegramService(nodecg, "telegram", __dirname, "../telegram-schema.json").register();
@@ -52,7 +36,7 @@ class TelegramService extends ServiceBundle<TelegramServiceConfig, TelegramServi
             webHook: config.webHook,
         };
 
-        const bot = new BetterTelegramBot(config.token, options);
+        const bot = new TelegramBot(config.token, options);
 
         this.nodecg.log.info("Successfully connected to the telegram server.");
 
