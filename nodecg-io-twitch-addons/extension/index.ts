dd8abf743e7f6ff2ac7eeb0b83600b24603dc8a2
noeppi_noeppi <noeppinoeppi@gmail.com>
2021-03-01 17:31:56 +0100

    Service for BTTV and FFZ

--- nodecg-io-twitch-addons/extension/index.ts
+++ nodecg-io-twitch-addons/extension/index.ts
@@ -1,36 +1,79 @@
 import { NodeCG } from "nodecg/types/server";
 import { Result, emptySuccess, success, ServiceBundle } from "nodecg-io-core";
 import { TwitchAddonsClient } from "./twitchAddonsClient";
+import fetch from "node-fetch";
 
 export interface TwitchAddonsConfig {
-    placeholder: string;
+    oauthKey: string;
 }
 
-export { TwitchAddonsClient } from "./twitchAddonsClient";
+export {
+    TwitchAddonsClient,
+    BetterTTVChannel,
+    BetterTTVEmote,
+    BetterTTVChannelEmote,
+    BetterTTVSharedEmote,
+    BetterTTVUser,
+    FFZUrl,
+    FFZChannel,
+    FFZRoom,
+    FFZEmoteSet,
+    FFZEmote,
+    FFZUser,
+    FFZGlobalEmotes,
+    EmoteCollection,
+    EmoteResolution,
+} from "./twitchAddonsClient";
 
 module.exports = (nodecg: NodeCG) => {
     new TwitchAddonsService(nodecg, "twitch-addons", __dirname, "../schema.json").register();
 };
 
 class TwitchAddonsService extends ServiceBundle<TwitchAddonsConfig, TwitchAddonsClient> {
-    async validateConfig(_: TwitchAddonsConfig): Promise<Result<void>> {
-        // TODO: Implement
+    async validateConfig(config: TwitchAddonsConfig): Promise<Result<void>> {
+        await TwitchAddonsService.getClientId(TwitchAddonsService.normalizeToken(config.oauthKey));
         return emptySuccess();
     }
 
     async createClient(config: TwitchAddonsConfig): Promise<Result<TwitchAddonsClient>> {
-        // TODO: Implement
-        const client = TwitchAddonsClient.createClient(config);
+        const normalizedToken = TwitchAddonsService.normalizeToken(config.oauthKey);
+        const client = TwitchAddonsClient.createClient(
+            await TwitchAddonsService.getClientId(normalizedToken),
+            normalizedToken,
+        );
         this.nodecg.log.info("Successfully created twitch-addons client.");
         return success(client);
     }
 
     stopClient(_: TwitchAddonsClient): void {
-        // TODO: Implement
         this.nodecg.log.info("Successfully stopped twitch-addons client.");
     }
 
     removeHandlers(_: TwitchAddonsClient): void {
         // TODO: Implement (optional)
+    }
+
+    private static normalizeToken(token: string): string {
+        if (token.toLowerCase().startsWith("oauth:")) {
+            return token.substr(6);
+        } else {
+            return token;
+        }
+    }
+
+    private static async getClientId(token: string): Promise<string> {
+        const response = await (
+            await fetch(`https://id.twitch.tv/oauth2/validate`, {
+                headers: {
+                    Authorization: "OAuth " + token,
+                },
+            })
+        ).json();
+        if (response.client_id == undefined) {
+            throw new Error(
+                "Failed to get client id for twitch token in nodecg-io-twitch-addons. Probably invalid token.",
+            );
+        }
+        return response.client_id;
     }
 }
--- samples/twitch-addons/extension/index.ts
+++ samples/twitch-addons/extension/index.ts
@@ -7,13 +7,17 @@ module.exports = function (nodecg: NodeCG) {
 
     const twitch_addons = requireService<TwitchAddonsClient>(nodecg, "twitch-addons");
 
-    twitch_addons?.onAvailable((_) => {
+    twitch_addons?.onAvailable(async (client) => {
         nodecg.log.info("Twitch-addons service available.");
-        // TODO: Implement
+        const emotes = await client.getEmoteCollection("#derniklaas", false);
+        const emoteNames = client.getEmoteNames(emotes);
+        const global = await client.getEmoteCollection("#derniklaas", true);
+        const globalNames = client.getEmoteNames(global);
+        nodecg.log.info(`BTTV & FFZ emotes on the twitch channel #derniklaas (without global emotes): ${emoteNames}`);
+        nodecg.log.info(`BTTV & FFZ emotes on the twitch channel #derniklaas (with global emotes): ${globalNames}`);
     });
 
     twitch_addons?.onUnavailable(() => {
         nodecg.log.info("Twitch-addons service unavailable.");
-        // TODO: Implement
     });
 };
