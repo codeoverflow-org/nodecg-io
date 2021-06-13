e762d2c3ca782c2134b355bacda858bbcbaf08f2
sebinside <hi@sebinside.de>
2021-06-13 20:08:13 +0200

    Streamline knex creation.

--- nodecg-io-sql/extension/index.ts
+++ nodecg-io-sql/extension/index.ts
@@ -1,37 +1,32 @@
 import { NodeCG } from "nodecg/types/server";
 import { Result, emptySuccess, success, ServiceBundle } from "nodecg-io-core";
-import { SQLClient } from "./sqlClient";
+import { Knex, knex } from "knex";
 
 export interface SQLConfig {
     client: string;
     connection: Record<string, unknown>;
 }
 
-export { SQLClient } from "./sqlClient";
-
 module.exports = (nodecg: NodeCG) => {
     new SQLService(nodecg, "sql", __dirname, "../schema.json").register();
 };
 
-class SQLService extends ServiceBundle<SQLConfig, SQLClient> {
+class SQLService extends ServiceBundle<SQLConfig, Knex> {
+    private knexInstance: Knex;
+
     async validateConfig(_: SQLConfig): Promise<Result<void>> {
-        // TODO: Implement
+        // No way to validate without creating the client
         return emptySuccess();
     }
 
-    async createClient(config: SQLConfig): Promise<Result<SQLClient>> {
-        // TODO: Implement
-        const client = SQLClient.createClient(config);
+    async createClient(config: SQLConfig): Promise<Result<Knex>> {
+        this.knexInstance = knex(config);
         this.nodecg.log.info("Successfully created sql client.");
-        return success(client);
+        return success(this.knexInstance);
     }
 
-    stopClient(_: SQLClient): void {
-        // TODO: Implement
+    stopClient(client: Knex): void {
+        client.destroy();
         this.nodecg.log.info("Successfully stopped sql client.");
-    }
-
-    removeHandlers(_: SQLClient): void {
-        // TODO: Implement (optional)
     }
 }
