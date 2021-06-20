5222739593f4522f918556fb700e7336784d9628
sebinside <hi@sebinside.de>
2021-06-20 22:50:08 +0200

    Add simple knex examples.

--- samples/sql/extension/index.ts
+++ samples/sql/extension/index.ts
@@ -7,10 +7,38 @@ module.exports = function (nodecg: NodeCG) {
 
     const sql = requireService<SQLCLient>(nodecg, "sql");
 
-    sql?.onAvailable((sqlClient) => {
+    sql?.onAvailable(async (sql) => {
+        // In the following, we demonstrate some simple examples on how to use knex.js
         nodecg.log.info("SQL service available.");
 
-        // TODO SH: Add some sample code incl. explanations
+        // Select the columns 'Id' and 'Content' from the table 'Test' (not typesafe)
+        (await sql("Test").select("Id", "Content")).forEach((row) =>
+            nodecg.log.info(`Received row with id: ${row.Id}`),
+        );
+
+        // Define an interface with the fields of the table for more type safety
+        interface TestItem {
+            Id: number;
+            Content: string;
+        }
+        (await sql<TestItem>("Test")).forEach((row) => nodecg.log.info(`Received content: ${row.Content}`));
+
+        // An example on how to use *where*
+        (await sql<TestItem>("Test2").where("Id", 3)).forEach((row) =>
+            nodecg.log.info(`Content of element with Id == 3: ${row.Content}`),
+        );
+
+        // An example on how to use *join*
+        (
+            await sql("Test")
+                .leftJoin("Test2", "Test.Id", "Test2.Id")
+                .select("Test.Content as Content1", "Test2.Content as Content2")
+        ).forEach((match) => nodecg.log.info(`Matching elements: '${match.Content1}' and '${match.Content2}'`));
+
+        // An example on how to use *insert* (we assume Id to be auto-incrementing and leave it empty)
+        await sql<TestItem>("Test2").insert({ Content: `Some random content: ${Math.random()}` });
+
+        // Way more examples can be found on knexjs.org
     });
 
     sql?.onUnavailable(() => {
