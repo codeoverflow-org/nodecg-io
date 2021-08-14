260ca76d8e7bbae68f6f737209145df57faae435
Daniel <daniel.hub@outlook.de>
2021-08-14 22:35:42 +0200

    Generate test coverage in CI and add coverage dir to gitignore

--- nodecg-io-core/jest.config.js
+++ nodecg-io-core/jest.config.js
@@ -2,4 +2,7 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   testMatch: ["<rootDir>/extension/__tests__/**/*.ts", "!**/extension/__tests__/mocks.ts"],
+  // Exclude mocks.ts from coverage. It doesn't matter because it is mock code not used
+  // but required to make TypeScript compile it.
+  coveragePathIgnorePatterns: ["<rootDir>/extension/__tests__/"]
 };
