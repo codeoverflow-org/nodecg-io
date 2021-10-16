d30a7414132628b4c26d9da68789c691bb11664f
Nils Witt <nils@nils-witt.de>
2021-10-16 21:30:19 +0200

    added mqtt client service and sample

---
+++ nodecg-io-mqtt-client/extension/index.ts
@@ -0,0 +1,98 @@
+import { NodeCG } from "nodecg-types/types/server";
+import { Result, emptySuccess, success, ServiceBundle, error } from "nodecg-io-core";
+import { MqttClient, connect } from "mqtt";
+
+interface MQTTClientServiceConfig {
+    address: string;
+    channels: [string];
+    username?: string;
+    password?: string;
+}
+
+export class MQTTClientServiceClient {
+    client: MqttClient;
+    once: (event: string, cb: any) => void;
+    close: () => void;
+    on: (event: string, cb: any) => void;
+    off: (event: string | symbol, listener: (...args: any[]) => void) => void;
+
+    connect(url: string, username: string | undefined, password: string | undefined): Promise<void> {
+        return new Promise((resolve, reject) => {
+            this.client = connect(url, {
+                username: username,
+                password: password,
+            });
+            this.client.on("error", (err: Error) => {
+                this.client.end();
+                reject(err.message);
+            });
+            this.client.on("connect", () => {
+                resolve();
+            });
+
+            this.once = this.client.once;
+            this.on = this.client.on;
+            this.close = this.client.end;
+            this.off = this.client.off;
+        });
+    }
+
+    subscribe(channels: string[]): void {
+        channels.forEach((channel: string) => {
+            this.client.subscribe(channel);
+        });
+    }
+
+    onClose(func: () => void): void {
+        this.client.on("close", () => {
+            return func();
+        });
+    }
+
+    onMessage(func: (topic: string, message: any) => void): void {
+        this.client.on("message", (topic, message) => {
+            return func(topic, message);
+        });
+    }
+
+    onError(func: (error: Error) => void): void {
+        this.client.on("error", (error: Error) => {
+            return func(error);
+        });
+    }
+}
+
+module.exports = (nodecg: NodeCG) => {
+    new MQTTClientService(nodecg, "mqtt-client", __dirname, "../mqtt-schema.json").register();
+};
+
+class MQTTClientService extends ServiceBundle<MQTTClientServiceConfig, MQTTClientServiceClient> {
+    async validateConfig(config: MQTTClientServiceConfig): Promise<Result<void>> {
+        this.nodecg.log.info("validation");
+        const client = new MQTTClientServiceClient();
+        try {
+            await client.connect(config.address, config.username, config.password);
+            return emptySuccess();
+        } catch (e) {
+            return error(e);
+        }
+    }
+
+    async createClient(config: MQTTClientServiceConfig): Promise<Result<MQTTClientServiceClient>> {
+        const client = new MQTTClientServiceClient();
+        await client.connect(config.address, config.username, config.password);
+        client.subscribe(config.channels);
+        this.nodecg.log.info("Successfully connected to the MQTT server.");
+        return success(client);
+    }
+
+    stopClient(client: MQTTClientServiceClient): void {
+        if (client.client.connected) {
+            client.close();
+        }
+    }
+
+    removeHandlers(client: MQTTClientServiceClient): void {
+        client.client.removeAllListeners();
+    }
+}
---
+++ samples/mqtt-client/extension/index.ts
@@ -0,0 +1,25 @@
+import { NodeCG } from "nodecg-types/types/server";
+import { requireService } from "nodecg-io-core";
+import { MQTTClientServiceClient } from "nodecg-io-mqtt-client";
+
+module.exports = function (nodecg: NodeCG) {
+    nodecg.log.info("Sample bundle for websocket-client started");
+
+    const service = requireService<MQTTClientServiceClient>(nodecg, "mqtt-client");
+    let interval: NodeJS.Timeout | undefined;
+
+    service?.onAvailable((client) => {
+        nodecg.log.info("Client has been updated, waiting for messages.");
+
+        client.onMessage((topic: string, message: any) => {
+            nodecg.log.info(`recieved message "${message.toString()}" "${topic}"`);
+        });
+    });
+
+    service?.onUnavailable(() => {
+        nodecg.log.info("Client has been unset.");
+        if (interval) {
+            clearInterval(interval);
+        }
+    });
+};
