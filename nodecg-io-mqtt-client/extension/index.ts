8f0d2fb3423bf7dd941beda1057879a9f5fab2cf
Nils Witt <nils@nilswitt.dev>
2021-10-17 17:09:50 +0200

    Add MQTT service & sample bundle (#265)
    * added mqtt client service and sample
    
    * fixed lint problems
    
    * updated schema
    
    * updated schema
    
    * updated package.json
    
    * updated package.json
    
    * Fixed to close the client connection after config validation
    
    * Removed unnecessary lines
    
    * Apply suggestions from code review
    
    Co-authored-by: Daniel Huber <daniel.hub@outlook.de>
    
    * Removed try catch and shortened callbacks
    
    * Harmonize event handler shortcuts in mqtt service
    
    Co-authored-by: Daniel Huber <daniel.hub@outlook.de>

---
+++ nodecg-io-mqtt-client/extension/index.ts
@@ -0,0 +1,87 @@
+import { NodeCG } from "nodecg-types/types/server";
+import { Result, emptySuccess, success, ServiceBundle, error } from "nodecg-io-core";
+import { MqttClient, connect } from "mqtt";
+
+interface MQTTClientServiceConfig {
+    address: string;
+    topics: [string];
+    username?: string;
+    password?: string;
+}
+
+export class MQTTClientServiceClient {
+    client: MqttClient;
+    once: (event: string, cb: () => void) => void;
+    close: () => void;
+    on: (event: string, cb: () => void) => void;
+    off: (event: string | symbol, listener: (...args: unknown[]) => void) => void;
+
+    connect(url: string, username: string | undefined, password: string | undefined): Promise<void> {
+        return new Promise((resolve, reject) => {
+            this.client = connect(url, {
+                username: username,
+                password: password,
+            });
+            this.client.on("error", (err: Error) => {
+                this.client.end();
+                reject(err.message);
+            });
+            this.client.on("connect", resolve);
+
+            this.once = this.client.once;
+            this.on = this.client.on;
+            this.close = this.client.end;
+            this.off = this.client.off;
+        });
+    }
+
+    subscribe(topics: string[]): void {
+        topics.forEach((topic: string) => {
+            this.client.subscribe(topic);
+        });
+    }
+
+    onClose(func: () => void): MqttClient {
+        return this.client.on("close", func);
+    }
+
+    onMessage(func: (topic: string, message: Buffer) => void): MqttClient {
+        return this.client.on("message", func);
+    }
+
+    onError(func: (error: Error) => void): MqttClient {
+        return this.client.on("error", func);
+    }
+}
+
+module.exports = (nodecg: NodeCG) => {
+    new MQTTClientService(nodecg, "mqtt-client", __dirname, "../mqtt-schema.json").register();
+};
+
+class MQTTClientService extends ServiceBundle<MQTTClientServiceConfig, MQTTClientServiceClient> {
+    async validateConfig(config: MQTTClientServiceConfig): Promise<Result<void>> {
+        const client = new MQTTClientServiceClient();
+
+        await client.connect(config.address, config.username, config.password);
+        client.close();
+        return emptySuccess();
+    }
+
+    async createClient(config: MQTTClientServiceConfig): Promise<Result<MQTTClientServiceClient>> {
+        const client = new MQTTClientServiceClient();
+        await client.connect(config.address, config.username, config.password);
+        client.subscribe(config.topics);
+        this.nodecg.log.info("Successfully connected to the MQTT server.");
+        return success(client);
+    }
+
+    stopClient(client: MQTTClientServiceClient): void {
+        if (client.client.connected) {
+            client.close();
+        }
+    }
+
+    removeHandlers(client: MQTTClientServiceClient): void {
+        client.client.removeAllListeners();
+    }
+}
