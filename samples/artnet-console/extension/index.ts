efdeda8d78a34e228b5fa6cba00636125cea3508
Tim_Tech_Dev <Tim_Tech_Dev@protonmail.com>
2021-10-22 16:13:49 +0200

    Fix  #269
    The interval in artnet-console tried to access the unset client.
    This caused a crash.
    
    Aso removed unused error import from mqtt-client (lint warning).
    
    Signed-off-by: Tim_Tech_Dev <Tim_Tech_Dev@protonmail.com>

--- samples/artnet-console/extension/index.ts
+++ samples/artnet-console/extension/index.ts
@@ -6,6 +6,9 @@ module.exports = function (nodecg: NodeCG) {
     nodecg.log.info("Sample bundle for Art-Net started");
 
     const service = requireService<ArtNetServiceClient>(nodecg, "artnet");
+    let value = 0;
+    let timeout: undefined | NodeJS.Timeout = undefined;
+
     service?.onAvailable((client) => {
         // From this point on is the artnet client available
         nodecg.log.info("Art-Net console has been updated, setting up interval for sending test payloads.");
@@ -17,8 +20,7 @@ module.exports = function (nodecg: NodeCG) {
         });
 
         // Send DMX data to every channel and universe.
-        let value = 0;
-        setInterval(() => {
+        timeout = setInterval(() => {
             // send new data every 0,8 seconds.
             // This is the official timing for re-transmiting data in the artnet specifciation.
             if (++value > 255) value = 0;
@@ -32,5 +34,11 @@ module.exports = function (nodecg: NodeCG) {
         }, 800);
     });
 
-    service?.onUnavailable(() => nodecg.log.info("Art-Net console has been unset."));
+    service?.onUnavailable(() => {
+        if (timeout) {
+            clearInterval(timeout);
+            value = 0;
+        }
+        nodecg.log.info("Art-Net console has been unset.");
+    });
 };
