codeoverflow-org/nodecg-io

commit de1c46d49e10fe29310526f8ec9320a18569495c

Load monaco-editor using webpack instead of monacos own loader (#312)
Closes #311.
Monaco uses some newer browser features that are not available on old chromium versions like the one embedded in OBS. To fix this (and also fix our kinda hacky loading of monaco through a relative fs mount to the root  `node_modules`) we can load monaco through webpack.
I've activated webpack caching because webpack is now actually processing more than 5 files and using it the build time can be reduced from ~6s to 1.5s per webpack project.

nodecg-io-core/dashboard/serviceInstance.ts
@@ -1,4 +1,4 @@
-/// <reference types="monaco-editor/monaco" />
+import * as monaco from "monaco-editor";
 import {
     CreateServiceInstanceMessage,
     DeleteServiceInstanceMessage,
@@ -33,8 +33,10 @@ const instancePreset = document.getElementById("instancePreset");
 const instanceNameField = document.getElementById("instanceNameField");
 const instanceEditButtons = document.getElementById("instanceEditButtons");
 const instanceCreateButton = document.getElementById("instanceCreateButton");
-const instanceMonaco = document.getElementById("instanceMonaco");
-let editor: monaco.editor.IStandaloneCodeEditor | undefined;
+const instanceMonaco = document.getElementById("instanceMonaco")!;
+const editor = monaco.editor.create(instanceMonaco, {
+    theme: "vs-dark",
+});
 
 const spanInstanceNotice = document.getElementById("spanInstanceNotice");
 const buttonSave = document.getElementById("buttonSave");
@@ -46,18 +48,6 @@ window.addEventListener("resize", () => {
 });
 export function updateMonacoLayout(): void {
     editor?.layout();
-}
-
-export function onMonacoReady(): void {
-    if (instanceMonaco) {
-        editor = monaco.editor.create(instanceMonaco, {
-            theme: "vs-dark",
-        });
-
-        // Virtually selects the same instance option again to show the json/text in the editor.
-        const selected = selectInstance.options[selectInstance.selectedIndex]?.value || "select";
-        selectServiceInstance(selected);
-    }
 }
 
 // Instance drop-down

services/nodecg-io-debug/dashboard/debug-helper.ts
@@ -1,5 +1,5 @@
 /// <reference types="nodecg-types/types/browser" />
-/// <reference types="monaco-editor/monaco" />
+import * as monaco from "monaco-editor";
 
 // Buttons
 for (let i = 1; i <= 5; i++) {
@@ -71,8 +71,13 @@ setHandler("#list_list_send", "onclick", () => {
 });
 
 // JSON
-// defined in debug-helper.html
-declare const debugMonacoEditor: monaco.editor.IStandaloneCodeEditor;
+const jsonCode = JSON.stringify({ data: 42 }, null, 4);
+const model = monaco.editor.createModel(jsonCode, "json");
+const debugMonacoEditor = monaco.editor.create(document.getElementById("instanceMonaco")!, {
+    model: model,
+    theme: "vs-dark",
+});
+
 setHandler("#json_send", "onclick", () => {
     const jsonString = debugMonacoEditor.getValue();
     try {

.gitignore
@@ -22,6 +22,8 @@ coverage
 # Dashboard webpack
 nodecg-io-core/dashboard/dist
 !nodecg-io-core/dashboard/webpack.config.js
+services/nodecg-io-debug/dashboard/dist
+!services/nodecg-io-debug/webpack.config.js
 
 # Editor configuration
 .idea/

nodecg-io-core/dashboard/main.ts
@@ -1,7 +1,6 @@
 // Re-exports functions that are used in panel.html
 export { loadFramework } from "./authentication";
 export {
-    onMonacoReady,
     onInstanceSelectChange,
     createInstance,
     saveInstanceConfig,

nodecg-io-core/dashboard/panel.html
@@ -91,12 +91,5 @@
 
         <!-- Scripts -->
         <script src="./dist/main.bundle.js"></script>
-
-        <!-- monaco is manually included because when including it using webpack it created formatting issues. -->
-        <script src="../modules/monaco-editor/min/vs/loader.js"></script>
-        <script>
-            require.config({ paths: { vs: "../modules/monaco-editor/min/vs" } });
-            require(["vs/editor/editor.main"], onMonacoReady);
-        </script>
     </body>
 </html>

services/nodecg-io-debug/dashboard/debug-helper.html
@@ -165,26 +165,9 @@
                 <div>
                     <button id="json_send">Send</button>
                 </div>
-
-                <script src="../modules/monaco-editor/min/vs/loader.js"></script>
-                <script>
-                    require.config({ paths: { vs: "../modules/monaco-editor/min/vs" } });
-                    require(["vs/editor/editor.main"], onMonacoReady);
-
-                    function onMonacoReady() {
-                        var jsonCode = ["{", '    "data": 42', "}"].join("\n");
-
-                        var model = monaco.editor.createModel(jsonCode, "json");
-
-                        window.debugMonacoEditor = monaco.editor.create(document.getElementById("instanceMonaco"), {
-                            model: model,
-                            theme: "vs-dark",
-                        });
-                    }
-                </script>
             </div>
         </div>
 
-        <script src="debug-helper.js"></script>
+        <script src="dist/main.bundle.js"></script>
     </body>
 </html>

services/nodecg-io-debug/webpack.config.js
@@ -3,14 +3,14 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
 
 const ROOT = path.resolve(__dirname);
-const DESTINATION = path.resolve(__dirname, 'dist');
+const DESTINATION = path.resolve(__dirname, 'dashboard', 'dist');
 
 module.exports = {
     context: ROOT,
 
     mode: "none",
     entry: {
-        'main': './main.ts'
+        'main': 'dashboard/debug-helper.js'
     },
 
     output: {
@@ -32,7 +32,7 @@ module.exports = {
     plugins: [
         new CleanWebpackPlugin(),
         new MonacoWebpackPlugin({
-            languages: ['text', 'json']
+            languages: ['json']
         })
     ],
 

commit ae8089c7b14b12ced2df57ae6342448a5430796b

Remove monaco null assertions to fix linter warnings
Introduced by #312. The null assertions were ment for temporary testing and I forgot to remove them....

nodecg-io-core/dashboard/serviceInstance.ts
@@ -33,7 +33,10 @@ const instancePreset = document.getElementById("instancePreset");
 const instanceNameField = document.getElementById("instanceNameField");
 const instanceEditButtons = document.getElementById("instanceEditButtons");
 const instanceCreateButton = document.getElementById("instanceCreateButton");
-const instanceMonaco = document.getElementById("instanceMonaco")!;
+const instanceMonaco = document.getElementById("instanceMonaco");
+if (instanceMonaco === null) {
+    throw new Error("Couldn't find instanceMonaco");
+}
 const editor = monaco.editor.create(instanceMonaco, {
     theme: "vs-dark",
 });

services/nodecg-io-debug/dashboard/debug-helper.ts
@@ -71,9 +71,14 @@ setHandler("#list_list_send", "onclick", () => {
 });
 
 // JSON
+const instanceMonaco = document.getElementById("instanceMonaco");
+if (instanceMonaco === null) {
+    throw new Error("Could not find instanceMonaco");
+}
+
 const jsonCode = JSON.stringify({ data: 42 }, null, 4);
 const model = monaco.editor.createModel(jsonCode, "json");
-const debugMonacoEditor = monaco.editor.create(document.getElementById("instanceMonaco")!, {
+const debugMonacoEditor = monaco.editor.create(instanceMonaco, {
     model: model,
     theme: "vs-dark",
 });