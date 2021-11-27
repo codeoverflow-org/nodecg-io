b834abc12d7f4e3336cc8378747d3838f448cbd3
Tim_Tech_Dev <Tim_Tech_Dev@protonmail.com>
2021-11-27 14:01:42 +0100

    feat/NodeCG-CI: Format ci.yml
    - Format ci.yml
    - Use `working-directory` feature inside run scripts
    - Add a bit of documentation
    
        Tim
    
    Signed-off-by: Tim_Tech_Dev <Tim_Tech_Dev@protonmail.com>

--- .scripts/ci-nodecg-integration.mjs
+++ .scripts/ci-nodecg-integration.mjs
@@ -5,8 +5,9 @@ import * as child_process from "child_process";
 const cwd = path.parse(process.cwd());
 const nodecgiodir = fs.readdirSync(process.cwd()).some((v) => v === "nodecg-io-core");
 
+// Check if we are in the nodecg-io folder because we use relative paths later on.
 if (!nodecgiodir) {
-    throw new Error("You will have to run this script from inside the nodecg-io folder");
+    throw new Error("You will have to run this script from inside the nodecg-io folder!");
 }
 /**
  * expected data:
@@ -25,7 +26,7 @@ if (!nodecgiodir) {
  */
 const npm = JSON.parse(child_process.execSync("npm ls --json"));
 
-// Filter out any dependencies whitch are not resolved locally in samples or services because the other npm packages wil not be loaded by NodeCG
+// Filter out any dependencies which are not resolved locally in samples or services because the other npm packages will not be loaded by NodeCG
 let bundles = Object.entries(npm.dependencies)
     .filter((i) =>
         Object.entries(i[1]).some(
@@ -43,18 +44,23 @@ console.log("");
 console.log("NodeCG sterr");
 console.log("--------------------------------------------------------------------------------");
 
-// expectes a NodeCG folder be the parent of the cwd needs timeout
+// expects a NodeCG folder be the parent of the cwd needs timeout
 const log = child_process
     .execSync("timeout --preserve-status 15s node " + cwd.dir + path.sep + "index.js", { cwd: cwd.dir })
     .toString("utf8");
 
 const lines = log.split("\n");
 
+
+// Try to find each bundle in the logs.
 const missing = bundles.filter(
+    /*Using endsWith here to remove possible ansi styling of "[info]" caused by ModeCG's logger when run locally*/
     (i) => !lines.some((j) => j.endsWith("[nodecg/lib/server/extensions] Mounted " + i + " extension")),
 );
 
+// Fail the run if there are missing bundles.
 if (missing.length > 0) {
+    // Only log stout if the run has failed because otherwise its unimportant and everything important should be in stderr
     console.log("");
     console.log("NodeCG stout");
     console.log("--------------------------------------------------------------------------------");
@@ -63,6 +69,7 @@ if (missing.length > 0) {
     console.log("");
     console.log("Missing Bundles");
     console.log("--------------------------------------------------------------------------------");
-    console.log(`NodeCG did not mount ${missing.length} bundle(s).`);
-    throw new Error(missing);
+    console.log(missing);
+
+    throw new Error(`NodeCG did not mount ${missing.length} bundle(s).`);
 }
