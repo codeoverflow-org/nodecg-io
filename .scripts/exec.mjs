47f418108b8e4ed9ce6930603b6be4c08dd82b37
Tim_Tech_Dev <Tim_Tech_Dev@protonmail.com>
2021-12-29 22:27:12 +0100

    Update build process
    - Update esbuild config to include a watch and rebuild option
    - Improved build script with easy to understand log outputs
    - New update process: You only need to specify your bundle once.
    
        Tim
    
    Signed-off-by: Tim_Tech_Dev <Tim_Tech_Dev@protonmail.com>

---
+++ .scripts/exec.mjs
@@ -0,0 +1,47 @@
+import { root, packages } from "./update-paths.mjs";
+import concurrently from "concurrently";
+
+const COMMAND = process.argv[2];
+
+/**@type {import('concurrently').CommandObj[]}*/
+const commands = packages
+    .filter((v) => v.packageJson["scripts"] && v.packageJson["scripts"][COMMAND])
+    .map((v) => ({
+        name: v.packageJson.name,
+        command: "npm:" + COMMAND,
+        cwd: v.dir,
+    }));
+
+const scripts = root.packageJson["scripts"];
+if (scripts && scripts[COMMAND + ":root"]) {
+    commands.unshift({
+        name: root.packageJson.name,
+        command: "npm:" + COMMAND + ":root",
+        cwd: root.dir,
+    });
+}
+
+const colors = [
+    "blue",
+    "green",
+    "cyan",
+    "magenta",
+    "red",
+    "yellow",
+    "white",
+    "gray",
+    "blackBright",
+    "redBright",
+    "greenBright",
+    "yellowBright",
+    "blueBright",
+    "magentaBright",
+    "cyanBright",
+    "whiteBright",
+];
+
+if (commands.length > 0) {
+    concurrently(commands, {
+        prefixColors: colors,
+    });
+}
