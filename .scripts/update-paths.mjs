c9e65bc3837b3f53d1002c3b04f080316baf88fd
Daniel Huber <30466471+daniel0611@users.noreply.github.com>
2023-02-01 13:08:23 +0100

    Update repository build scripts for manypkg utils v2

--- .scripts/update-paths.mjs
+++ .scripts/update-paths.mjs
@@ -3,14 +3,14 @@ import path from "path";
 
 import { getPackages } from "@manypkg/get-packages";
 
-export const { root, packages } = await getPackages(process.cwd());
+export const { rootDir, packages, rootPackage } = await getPackages(process.cwd());
 
-const rootTSconfig = path.join(root.dir, "tsconfig.json");
+const rootTSconfig = path.join(rootDir, "tsconfig.json");
 const tsconfig = {
     files: [],
     references: packages
         .filter((pkg) => fs.readdirSync(pkg.dir).includes("tsconfig.json"))
-        .map((v) => ({ path: path.relative(root.dir, v.dir) })),
+        .map((v) => ({ path: path.relative(rootDir, v.dir) })),
 };
 
 let content = "// This file will be overwritten automatically! Do not store options here.\n" + JSON.stringify(tsconfig);
